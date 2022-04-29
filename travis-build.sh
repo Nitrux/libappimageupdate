bbbac8f25c38bc3aef7e1968323a4fbc22dc8332
Uri Herrera <hello@uriherrera.com>
2022-04-29 04:15:18 -0500

    ...

--- travis-build.sh
+++ travis-build.sh
@@ -30,6 +30,7 @@ DEBIAN_FRONTEND=noninteractive apt -qq -yy install --no-install-recommends \
 	libssh2-1-dev \
 	libssl-dev \
 	desktop-file-utils \
+	libcurl4-nss-dev \
 	libfuse-dev \
 	zlib1g-dev
 
