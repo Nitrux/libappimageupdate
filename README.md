d6c29bd14e705d8d8019d31fdd67af37519c8ff5
Uri Herrera <hello@uriherrera.com>
2022-04-28 21:14:42 -0500

    create pkg

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
-# libappimageupdate
-AppImageUpdate Library
+# libappimageupdate [![Build Status](https://app.travis-ci.com/Nitrux/libappimageupdate.svg?branch=main)](https://app.travis-ci.com/Nitrux/libappimageupdate)
+
+AppImageUpdate Library.
+
+# Issues
+If you find problems with the contents of this repository please create an issue.
+
+©2022 Nitrux Latinoamericana S.C.
---
+++ travis-build.sh
@@ -0,0 +1,75 @@
+#!/bin/bash
+
+set -x
+
+### Update Sources
+
+DEBIAN_FRONTEND=noninteractive apt -qq update
+
+### Install Build Tools #1
+
+DEBIAN_FRONTEND=noninteractive apt -qq -yy install --no-install-recommends \
+	 git \
+	 cmake \
+	 checkinstall \
+	 g++
+
+### Install Package Build Dependencies #2
+
+DEBIAN_FRONTEND=noninteractive apt -qq -yy install --no-install-recommends \
+	zlib1g-dev
+
+### Clone repo.
+
+git clone --single-branch --branch master https://github.com/AppImage/AppImageUpdate.git
+git submodule update --init --recursive
+
+mv AppImageUpdate/ libappimageupdate/
+
+ls -l libappimageupdate/
+
+### Compile Source
+
+mkdir -p libappimageupdate/build && cd libappimageupdate/build
+
+cmake \
+	-DCMAKE_INSTALL_PREFIX=/usr \
+	-DCMAKE_BUILD_TYPE=Release \
+	-DCMAKE_INSTALL_SYSCONFDIR=/etc \
+	-DCMAKE_INSTALL_LOCALSTATEDIR=/var \
+	-DCMAKE_FIND_PACKAGE_NO_PACKAGE_REGISTRY=ON \
+	-DCMAKE_INSTALL_RUNSTATEDIR=/run "-GUnix Makefiles" \
+	-DCMAKE_VERBOSE_MAKEFILE=ON \
+	-DCMAKE_INSTALL_LIBDIR=lib/x86_64-linux-gnu ..
+
+make -j$(nproc)
+
+### Run checkinstall and Build Debian Package
+### DO NOT USE debuild, screw it
+
+>> description-pak printf "%s\n" \
+	'AppImageUpdate lets you update AppImages.' \
+	'' \
+	'Update AppImage using information embedded in the AppImage itself.' \	
+	'' \
+	''
+
+checkinstall -D -y \
+	--install=no \
+	--fstrans=yes \
+	--pkgname=libappimageupdate \
+	--pkgversion=2.0.0-alpha-1-20220124 \
+	--pkgarch=amd64 \
+	--pkgrelease="1" \
+	--pkglicense=LGPL-3 \
+	--pkggroup=libs \
+	--pkgsource=libappimageupdate \
+	--pakdir=../.. \
+	--maintainer=uri_herrera@nxos.org \
+	--provides=libappimageupdate \
+	--requires="libc6,zlib1g" \
+	--nodoc \
+	--strip=no \
+	--stripso=yes \
+	--reset-uids=yes \
+	--deldesc=yes
